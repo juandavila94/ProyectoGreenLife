6842405d56c13a1c9ed25f0497a9ddffcc60b641
Martin <Martin@DESKTOP-HA3LA13>
2017-06-26 18:50:01 -0500

    clsDatosFactura

---
+++ CapaDatos_GreenLife/clsDatosFactura.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapaDatos_GreenLife
+{
+    class clsDatosFactura
+    {
+        GreenlifeEntities bd = new GreenlifeEntities();
+        public void InsertarFactura(int idUsuario, int idCliente, DateTime fecha, string metodoDePago, decimal subtotal, decimal iva, decimal total )
+        {
+            using (var bd = new GreenlifeEntities())
+            {
+                var courseList = bd.spInsertarFactura( idUsuario,  idCliente,  fecha,  metodoDePago,  subtotal,  iva,  total);
+                bd.SaveChanges();
+            }
+        }
+
+        public object ConsultarFacturasDia(DateTime dia)
+        {
+            return bd.spConsultarFacturasDia(dia).ToList();
+        }
+
+        public object ConsultarFacturacionDiaria(DateTime dia)
+        {
+            return bd.spConsultarFacturacionDiaria(dia).ToList();
+        }
+
+        public object ConsultarFacturasPorCliente(int id)
+        {
+            return bd.spConsultarFacturasPorCliente(id).ToList();
+        }
+
+        public object ConsultarFacturasPorUsuario(int id)
+        {
+            return bd.spConsultarFacturasPorUsuario(id).ToList();
+        }
+    }
+}
