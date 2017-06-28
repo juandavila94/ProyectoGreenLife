29363c8f31c4c725a4cb94e91f0b2f8c8be33518
Martin <mfbeltran@udlanet.ec>
2017-06-28 00:13:45 -0500

    Cambios varios 2

--- GreenLife/frmUsuarios.Designer.cs
+++ GreenLife/frmUsuarios.Designer.cs
@@ -35,10 +35,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(962, 501);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmUsuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Usuarios";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
